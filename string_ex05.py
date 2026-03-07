0170cfc8b0a8fe850fd2609aba89f5ccfb4413d0
Diêgo Souza <142118500+diegops17@users.noreply.github.com>
2026-03-07 20:37:35 +0000

    resolução ex03, ex04 e ex05

---
+++ string_ex05.py
@@ -0,0 +1,17 @@
+'''
+Exercício 5: Personalização de E-mail de Marketing (Setor de Marketing) O marketing
+quer enviar um e-mail de boas-vindas. O cliente forneceu o nome completo: lucas
+ferreira souza. Você deve extrair apenas o primeiro nome para usar na saudação (ex:
+"Olá, Lucas!"). O código deve:
+1. Encontrar a posição do primeiro espaço.
+2. Fatiar o texto para pegar apenas o primeiro nome.
+3. Formatar o nome com a primeira letra maiúscula.
+4. Exibir a mensagem: "Olá, [Primeiro Nome], seja bem-vindo ao nosso clube!"
+'''
+
+nome = 'lucas ferreira souza'
+nome = nome.strip().title().split()
+primeiro_nome = nome[0]
+
+saudacao = f'Olá, {primeiro_nome}, seja bem-vindo ao nosso clube!'
+print(saudacao)
